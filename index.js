fdd0de0c051bb8299b8f872ff46421176b18fff7
Andreas Nedbal <andy@pixelde.su>
2020-03-25 01:17:39 +0100

    :sparkles: add execution of given commands as CLI arguments
    Resolves #4

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 const ConfigurationCollector = require('./lib/configuration');
+const CommandCollector = require('./lib/commands');
 
 module.exports = class Lakeside {
   constructor() {
     this.configuration = new ConfigurationCollector();
+    this.commands = new CommandCollector(this.configuration);
+
+    let cliArguments = process.argv.slice(2);
+    if (cliArguments.includes('--config')) {
+      cliArguments.splice(cliArguments.indexOf('--config'), 2);
+    }
+
+    this.run(cliArguments);
+  }
+
+  run(commands) {
+    let that = this;
+    const promises = [];
+
+    commands.forEach(function (commandArgument) {
+      const commandKeys = Object.keys(that.commands);
+
+      if (commandKeys.includes(commandArgument)) {
+        promises.push(
+          (new that.commands[commandArgument](that)).runCommand()
+        );
+      }
+      else {
+        throw new Error(`Command '${commandArgument}' does not exist!`);
+      }
+    });
+
+    Promise.all(promises).then();
   }
 }
